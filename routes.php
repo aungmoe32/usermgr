93ca26b66f295ed8178de90cdfaf581393cc6f96
Aung Moe Myint Thu <aungmoemyintthu@gmail.com>
2026-01-20 11:08:22 +0630

    add auth middlewares to routes

--- routes.php
+++ routes.php
@@ -9,20 +9,20 @@ $router->get('/', 'home.php');
 // Authentication
 $router->get('/login', 'auth/loginForm.php');
 $router->post('/login', 'auth/login.php');
-$router->post('/logout', 'auth/logout.php');
+$router->post('/logout', 'auth/logout.php', ['authenticated']);
 
 // Users
 $router->get('/users', 'users/index.php', ['authenticated']);
-$router->get('/users/create', 'users/create.php');
-$router->post('/users/store', 'users/store.php');
-$router->get('/users/edit', 'users/edit.php');
-$router->post('/users/update', 'users/update.php');
-$router->post('/users/delete', 'users/delete.php');
+$router->get('/users/create', 'users/create.php', ['authenticated']);
+$router->post('/users/store', 'users/store.php', ['authenticated']);
+$router->get('/users/edit', 'users/edit.php', ['authenticated']);
+$router->post('/users/update', 'users/update.php', ['authenticated']);
+$router->post('/users/delete', 'users/delete.php', ['authenticated']);
 
 // Roles
-$router->get('/roles', 'roles/index.php');
-$router->get('/roles/create', 'roles/create.php');
-$router->post('/roles/store', 'roles/store.php');
-$router->get('/roles/edit', 'roles/edit.php');
-$router->post('/roles/update', 'roles/update.php');
-$router->post('/roles/delete', 'roles/delete.php');
+$router->get('/roles', 'roles/index.php', ['authenticated']);
+$router->get('/roles/create', 'roles/create.php', ['authenticated']);
+$router->post('/roles/store', 'roles/store.php', ['authenticated']);
+$router->get('/roles/edit', 'roles/edit.php', ['authenticated']);
+$router->post('/roles/update', 'roles/update.php', ['authenticated']);
+$router->post('/roles/delete', 'roles/delete.php', ['authenticated']);
